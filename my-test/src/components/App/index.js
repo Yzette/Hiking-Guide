e2df6ad4fe02cf85334884d31ad4b3b2d4f53d36
alexa <alexa@localhost>
2021-12-01 22:29:45 +0100

    add second filter but still have a problem with de line bottom of first filter

--- my-test/src/components/App/index.js
+++ my-test/src/components/App/index.js
@@ -4,6 +4,7 @@ import sky from '../../assets/images/sky.png';
 import mountain from '../../assets/images/mountain.png';
 import hill from '../../assets/images/hill.png';
 import filter1 from '../../assets/images/filter1.png';
+import filter2 from '../../assets/images/filter2.png';
 
 import Header from '../Header';
 import Footer from '../Footer';
@@ -15,6 +16,7 @@ function App() {
       <img className="app_image2" src={mountain} alt="mountain picture" />
       <img className="app_image3" src={hill} alt="hill picture" />
       <img className="app_filter1" src={filter1} alt="filter on pictures" />
+      <img className="app_filter2" src={filter2} alt="an other filter on pictures" />
       <Header />
       <Footer />
     </div>
