0413a19e9c6f074b144b4718aaf1a76fa27ef869
alexa <alexa@localhost>
2021-12-02 15:29:08 +0100

    first content placed. Still have one problem with the number position compared to the text

---
+++ my-test/src/components/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import FirstContent from "./FirstContent";
+
+import './home.scss';
+
+const Home = () => (
+    <main className="home">
+        <FirstContent />
+    </main>
+);
+
+export default Home;
