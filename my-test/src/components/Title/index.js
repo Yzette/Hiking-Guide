6e7e597ded8969ddda9014eeea6a576f7358adfd
alexa <alexa@localhost>
2021-12-02 10:37:09 +0100

    add and organization of title's elements

--- my-test/src/components/Title/index.js
+++ my-test/src/components/Title/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import SocialMedia from "./SocialMedia";
+import MainTitle from "./MainTitle";
+import Slider from "./Slider";
 
 import './title.scss';
 
 const Title = () => (
     <div className="title">
         <SocialMedia />
+        <MainTitle />
+        <Slider />
     </div>
 );
 
