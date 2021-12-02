5e9c03b01bdd45566c7cfee296f1c66c57218671
alexa <alexa@localhost>
2021-12-02 15:53:11 +0100

    second and third content placed

--- my-test/src/components/Home/index.js
+++ my-test/src/components/Home/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 import FirstContent from "./FirstContent";
+import SecondContent from "./SecondContent";
+import ThirdContent from "./ThirdContent";
+
 
 import './home.scss';
 
 const Home = () => (
     <main className="home">
         <FirstContent />
+        <SecondContent />
+        <ThirdContent />
     </main>
 );
 
